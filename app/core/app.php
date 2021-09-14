e3146e9f4621c163204a6d873b077b1e51d0b97d
Roni-Boiz <2019cs155@stu.ucsc.lk>
2021-09-14 16:24:25 +0530

    update app.php to filter url

--- app/core/app.php
+++ app/core/app.php
@@ -3,7 +3,31 @@
     class app{
         function __construct(){
             
-            echo "hello";
+            $url = $this->getUrl();
+            if(file_exists($url[0])){
+                $this->controller = $url[0];
+                unset($url[0]);
+            }
+
+            require_once '../app/controllers/'.$this->controller.'.php';
+
+            if($url[1]){
+                if(method_exists($this->controller,$url[1])){
+                    $this->method = $url[1];
+                    unset($url[1]);
+                }
+            }
+
+            $cont = new $this->controller;
+            call_user_func_array([$cont,$this->method],$this->params);
+        }
+
+        private function getUrl(){
+            if(isset($_GET['url'])){
+                $url = explode('/',filter_var(rtrim($_GET['url'],'/'), FILTER_SANITIZE_URL));
+                return $url;
+            }
+            
         }
     }
 ?>
