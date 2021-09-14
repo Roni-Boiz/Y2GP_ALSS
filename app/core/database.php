0ec2cd9b307d20613e9735949502557ff448f5c3
Roni-Boiz <2019cs155@stu.ucsc.lk>
2021-09-14 16:34:58 +0530

    try pdo connection

--- app/core/database.php
+++ app/core/database.php
@@ -1,5 +1,19 @@
 <?php
 
+// class Database extends PDO{
+
+//     function __construct($DB_TYPE, $DB_HOST, $DB_NAME, $DB_USER, $DB_PASSWORD)
+//     {
+//         parent::__construct($DB_TYPE.':host='.$DB_HOST.';dbname='.$DB_NAME, $DB_USER, $DB_PASSWORD);
+//     }
+
+//     public function runQuery($query){
+//         $stmp = $this->prepare($query);
+//         $stmp->execute();
+//         return $stmp->fetchAll();
+//     }
+// }
+
 $servername = "localhost";
 $username = "root";
 $password = "";
