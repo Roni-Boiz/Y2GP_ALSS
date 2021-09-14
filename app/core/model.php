36255fb2b2572f13a89f625cc72d5ed75d00380e
Roni-Boiz <2019cs155@stu.ucsc.lk>
2021-09-14 16:33:29 +0530

    try pdo

--- app/core/model.php
+++ app/core/model.php
@@ -1,9 +1,12 @@
 <?php
 
+// require_once '../app/core/database.php';
 
 class model{
 
     function __construct(){
+        
+        // $this->db = new database(DB_TYPE, DB_HOST, DB_NAME, DB_USER, DB_PASSWORD);
         
         $this->conn = $this->getConnection();
     }
