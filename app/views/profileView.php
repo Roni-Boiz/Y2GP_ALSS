9128b99b517a19e9d7e4c428ec5d779ca7f8e1b4
Chathura manohara <Chathura manohara>
2021-09-16 00:06:48 +0530

    profile-view profile

--- app/views/profileView.php
+++ app/views/profileView.php
@@ -5,7 +5,54 @@
 ?>
 </head>
 <body style="background-color: gray; background-image:none;">
+    <div style="display:grid;grid-template-columns:230px 1fr" id="expand">
+    <div id="hh" class="hawlockhead" ><img src="../../public/img/image.png" alt="" id="logo"/><h1 id="title">Hawlock <span id="city">City</span></h1></div>
+    <div class="hawlockbody" id="hb"> 
+    <i class="fa fa-user-circle" style="font-size: 75px; padding:0"></i>
+    <input type="submit" onclick = "funedit()" value="Edit Profile">       
+        <h4 style="margin-left: 100px;"><?php
+        if ($this->users->num_rows > 0){
+                while($row = $this->users->fetch_assoc()){
+                        echo $row["apartment_no"];?></h4>
+                            <!-- view -->
+                            <!-- for all -->
+                            <form action="#" class="form1" id="view">
+                                <label for="fname">First Name</label><br>
+                                <input type="text" id="fname" name="firstname" class="input-field" placeholder=<?php echo $row["fname"] ?> READONLY><br>
 
+                                <label for="lname">Last Name</label><br>
+                                <input type="text" id="lname" name="lastname" class="input-field" placeholder=<?php echo $row["lname"] ?> READONLY><br>
+                                
+                                <label for="fname">NIC</label><br>
+                                <input type="text" id="nic" name="nic" class="input-field" placeholder=<?php echo $row["nic"] ?> READONLY><br>
+                                
+                                <label for="fname">Contact</label><br>
+                                <input type="text" id="phone_no" name="phone_no" class="input-field" placeholder=<?php echo $row["phone_no"] ?> READONLY><br>
+                                
+                                <label for="lname">Email</label><br>
+                                <input type="text" id="email" name="email" class="input-field" placeholder=<?php echo $row["email"] ?> READONLY><br>
+                                <!-- for resident -->
+                                <?php if($type=="re"){?>
+                                <label for="lname">Vehicle NO</label><br>
+                                <input type="text" id="vehicle_no" name="vehicle_no" class="input-field" placeholder=<?php echo $row["vehicle_no"] ?> READONLY><br>
+                                
+                                <label for="lname">Family Members</label><br>
+                                <input type="text" id="fam" name="fam" class="input-field" placeholder=<?php echo ""?> READONLY><br>
+
+                                <label for="lname">Balance</label><br>
+                                <input type="text" id="balance" name="balance" class="input-field" placeholder=<?php echo $row["balance"] ?> READONLY><br>
+                                <?php
+                                }?>
+                                <!-- ------------ -->    
+                            </form>
+                            <?php
+                }
+        }else{
+            echo "0 results";
+        }
+        ?>
+    </div> <!-- .hawlockbody div closed here -->
+</div> <!-- .expand div closed here -->
 
 </body>
 </html>
