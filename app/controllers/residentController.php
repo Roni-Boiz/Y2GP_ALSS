d19108339fbf230afafecb235f1452bfc2bbfb78
Chathura manohara <Chathura manohara>
2021-09-21 01:35:02 +0530

    add resident controller

---
+++ app/controllers/residentController.php
@@ -0,0 +1,21 @@
+<?php
+
+require_once '../app/core/controller.php';
+
+class residentController extends controller{
+
+    function __construct(){
+        parent::__construct();
+    }
+
+    public function index(){
+        echo 'I am home 123';
+    }
+
+    // view profile
+    public function profile(){
+        $this->loadModel('profileModel');
+        $this->view->users = $this->model->readTable();
+        $this->view->render('resident/profileView');
+    }
+}
