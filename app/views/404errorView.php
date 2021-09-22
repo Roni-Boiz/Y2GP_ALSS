1251bb1e1dec9e4f3486536530bc5a4f689adbb8
Roni-Boiz <2019cs155@stu.ucsc.lk>
2021-09-22 11:41:33 +0530

    small update display errors

--- app/views/404errorView.php
+++ app/views/404errorView.php
@@ -1,30 +1,36 @@
 <!DOCTYPE html>
 <html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-    <style>
-        p{
-            font-size:48px;
-            color: #2a225a;
-        }
 
-        .center {
-            margin: auto;
-            width: 20%;
-            padding: 10px;
-        }
-    </style>
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+  <style>
+    p {
+      font-size: 32px;
+      color: #2a225a;
+    }
+
+    .center {
+      margin: auto;
+      width: 20%;
+      padding: 10px;
+      word-wrap: normal;
+      font-size: 25px;
+      color: #2a225a;
+    }
+  </style>
 </head>
+
 <body>
 
   <div class="center">
-  <p>Error code 404</p>
     <p>Page not found</p>
+    <div>Error Message : <?= $error_message ?></div>
   </div>
+  
 
-    
 </body>
+
 </html>
