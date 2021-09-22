c719e4f829b608b0e84954028bbc50a5953008d3
Roni-Boiz <2019cs155@stu.ucsc.lk>
2021-09-22 13:33:42 +0530

    add models to each user

---
+++ app/models/managerModel.php
@@ -0,0 +1,21 @@
+<?php
+
+require '../app/core/model.php';
+
+class managerModel extends model {
+    function __construct(){
+         parent::__construct();
+    }
+
+    public function readTable(){
+        require '../app/core/database.php';
+        $sql = "SELECT * FROM user_account";
+        $result = $this->conn->query($sql);   
+        return $result;
+
+        // $sql = "SELECT * FROM user_account";
+        // $result = $this->db->runQuery($sql);   
+        // return $result;
+    }
+
+}
