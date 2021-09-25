81a017f39d08ccbc5dc63f9303605ef42f4605d7
Yashi6633 <nishathyasintha@gmail.com>
2021-09-25 13:56:55 +0530

    All logouts set

--- app/controllers/homeController.php
+++ app/controllers/homeController.php
@@ -44,7 +44,19 @@ class homeController extends controller{
         }
     }
 
+    public function logout(){
 
+        session_start();
+        $_SESSION = array();
+
+        if (isset($_COOKIE[session_name()])) {
+            setcookie(session_name(), '' , time() -86400, '/');
+        }
+
+        session_destroy();
+
+        $this->view->render('homeView');
+    }
 
 
 
