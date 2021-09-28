1bc098115706862d5658277a7fabf3c98e3dd9fd
Roni-Boiz <2019cs155@stu.ucsc.lk>
2021-09-28 23:47:52 +0530

    add sliding add announcement form

--- app/views/admin/announcementView.php
+++ app/views/admin/announcementView.php
@@ -1,38 +1,84 @@
 <?php
 include_once 'sidenav.php';
 ?>
-<style>
-    .announcement {
-        grid-column: 1/span 3;
-        box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2);
-        width: 100%;
-        margin: auto;
-        margin-top: 10px;
-        text-align: center;
-        font-family: arial;
-    }
 
-    .announcement .title {
-        color: grey;
-        font-size: 15px;
-    }
-</style>
 </head>
 
 <body style="background-color: gray; background-image:none;">
+
     <div style="display:grid;grid-template-columns:230px 1fr" id="expand" class="content">
 
-        <div id="hh" class="hawlockhead"><img src="../../public/img/image.png" alt="" id="logo" />
+        <div id="hh" class="hawlockhead">
+            <img src="../../public/img/image.png" alt="" id="logo" />
             <h1 id="title">Hawlock <span id="city">City</span></h1>
+
         </div>
+
         <div id="hb" class="hawlockbody">
             <h2>Announcements</h2>
-            <?php
-            if ($this->ann->num_rows > 0) {
-                $count = 4;
-                while ($row = $this->ann->fetch_assoc()) { ?>
-                    <div class="card">
+            <!-- Add Anouncement -->
+            <div id="myCanvasNav" class="overlay" onclick="closeOffcanvas()" style="width: 0%; opacity: 0;"></div>
 
+            <div id="mySidenav" class="sidenav">
+                <a href="javascript:void(0)" class="closebtn" onclick="closeOffcanvas()">&times;</a>
+
+
+                <form action="addAnnouncement" class="formAddAnnouncement" method="get" enctype="multipart/form-data">
+                    <h1>Add announcement</h1>
+                    <br>
+                    <label for="topic">Topic</label><br>
+                    <input type="text" id="topic" name="topic" class="input-field" placeholder="New Announcement"><br>
+
+                    <label for="fname">Content</label><br>
+                    <textarea id="content" name="content" cols="30" rows="7" placeholder="Your content"></textarea><br>
+
+                    <label for="visibility">Visibility</label><br>
+                    <div class="visibility">
+                        <label for="resient">Resident</label><br>
+                        <input type="radio" id="resident" name="visibility" value="resident">
+                        <label for="admin">Administration</label><br>
+                        <input type="radio" id="administration" name="visibility" value="administration">
+                        <label for="both">Both</label><br>
+                        <input type="radio" id="both" name="visibility" value="both">
+                    </div>
+
+                    <label for="file">Add File</label><br>
+                    <input type="file" id="file" name="file" multiple>
+                    <br><br>
+
+                    <input type="submit" name="broadcast" value="Broadcast">
+                </form>
+            </div>
+
+            <div class="btnAddAnnouncement" onclick="openNav(); openOffcanvas()" title="Add Anouncement"><i class="fa fa-plus-square"></i></div>
+            <script>
+                function openOffcanvas() {
+                    document.getElementById("mySidenav").style.width = "400px";
+                    document.getElementById("hh").style.marginRight = "410px";
+                    document.getElementById("hb").style.marginRight = "410px";
+                }
+
+                function openNav() {
+                    document.getElementById("myCanvasNav").style.width = "100%";
+                    document.getElementById("myCanvasNav").style.opacity = "0.8";
+                }
+
+                function closeOffcanvas() {
+                    document.getElementById("mySidenav").style.width = "0%";
+                    document.getElementById("hh").style.marginRight = "10px";
+                    document.getElementById("hb").style.marginRight = "10px";
+                    document.getElementById("myCanvasNav").style.width = "0%";
+                    document.getElementById("myCanvasNav").style.opacity = "0";
+                }
+            </script>
+
+            <!-- Loading Announcements -->
+            <div class="card">
+                <?php
+                if ($this->ann->num_rows > 0) {
+                    $count = 4;
+                    while ($row = $this->ann->fetch_assoc()) {
+                ?>
                         <div class="card-body">
                             <div class="detail">
                                 <img src="../../public/img/user.png" alt="user" />
@@ -49,15 +95,15 @@ include_once 'sidenav.php';
                             <img src="../../public/img/<?php echo $count ?>.jpg">
                         </div>
                 <?php
+                    }
+                } else {
+                    echo "0 results";
                 }
-            } else {
-                echo "0 results";
-            }
                 ?>
                 <br>
 
 
-                    </div> 
+            </div>
         </div> <!-- .hawlockbody div closed here -->
 </body>
 
