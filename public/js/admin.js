db7096bc089c41661d1b416b4285f68ab247bcec
Roni-Boiz <2019cs155@stu.ucsc.lk>
2021-09-30 14:39:08 +0530

    add popup function scripts

--- public/js/admin.js
+++ public/js/admin.js
@@ -23,3 +23,45 @@ function closeOffcanvas() {
     document.getElementById("myCanvasNav").style.width = "0%";
     document.getElementById("myCanvasNav").style.opacity = "0";
 }
+/////////////////////////////////////////////////////////////////
+
+// popup model JS
+const model = document.getElementById("model");
+const modelBtn = document.getElementById("model-btn");
+const ans = document.getElementById("answer");
+const closeBtn = document.getElementById("closebtn");
+
+modelBtn.addEventListener("click", ()=>{
+    // document.getElementById("myCanvasNav").style.width = "100%";
+    // document.getElementById("myCanvasNav").style.opacity = "0.8";
+    model.className = "open";
+})
+
+closeBtn.addEventListener("click", ()=>{
+    model.className = 'close';
+    // document.getElementById("myCanvasNav").style.width = "0%";
+    // document.getElementById("myCanvasNav").style.opacity = "0";
+})
+
+model.addEventListener("click", (e)=>{
+    if(e.target.id === "yes-btn"){
+        ans.innerText = "Hello Guys";
+
+    }else if(e.target.id === "no-btn"){
+        ans.innerText = "Oh no! ";
+    }else{
+        return;
+    }
+    model.className = 'close';
+})
+
+// function loadData() {
+//     var cookies = document.cookie;
+//     console.log(cookies);
+//     // document.getElementById("emptype").setAttribute('value','Manager');
+//     document.getElementById("fname").setAttribute('value',1);
+
+//     // document.getElementById("lname").setAttribute('value',);
+//     // document.getElementById("email").setAttribute('value',);
+//     // document.getElementById("cno").setAttribute('value',);
+// }
