d5a4ec2a88bc00e05fb16ad9318eb1a9d633ca88
Roni-Boiz <2019cs155@stu.ucsc.lk>
2021-09-30 18:50:48 +0530

    add insert announcemnt function

--- app/models/adminModel.php
+++ app/models/adminModel.php
@@ -20,10 +20,24 @@ class adminModel extends model {
     public function getDetails(){
         session_start();
         $id = $_SESSION('user_id');
-        $sql = "SELECT * FROM admin where user_id='{$id}'";
+        $sql = "SELECT * FROM admin WHERE user_id='{$id}'";
         $result = $this->conn->query($sql);   
-        printf(mysqli_fetch_assoc($result));
         return $result;
+    }
+
+    public function insertAnnouncement($topic,$content,$category,$fileName,$id){
+        $admin = $this->conn->query("SELECT admin_id FROM admin WHERE user_id='{$id}'");
+        if($admin){
+            $id= mysqli_fetch_assoc($admin);
+            $adminId = $id['admin_id'];
+        }
+        $sql = "INSERT INTO announcement(topic,content,category,date,file_name,admin_id) VALUES ('{$topic}','{$content}','{$category}',NOW(),'{$fileName}','{$adminId}')";
+        return $this->conn->query($sql);
+    }
+
+    public function getEmployee($empId){
+        $sql = "SELECT * FROM manager WHERE employee_id='{$empId}'";
+        return $this->conn->query( $sql);
     }
 }
 ?>
