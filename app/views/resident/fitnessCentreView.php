e02f5e5c92da91cd0cd4a940a651b42294a603d8
Chathura <chathura.madura11@gmail.com>
2021-10-02 02:02:34 +0530

    fitness centre front end

--- app/views/resident/fitnessCentreView.php
+++ app/views/resident/fitnessCentreView.php
@@ -1,14 +1,69 @@
-<?php 
-    include_once 'sidenav.php';
+<?php
+include_once 'sidenav.php';
 ?>
 </head>
+
 <body style="background-color: gray; background-image:none;">
-<div style="display:grid;grid-template-columns:230px 1fr" id="expand" class="content">
+    <div style="display:grid;grid-template-columns:230px 1fr" id="expand" class="content">
 
-    <div id="hh" class="hawlockhead" ><img src="../../public/img/image.png" alt="" id="logo"/><h1 id="title">FITNESS CENTRE<br> <span id="city">RESERVATION</span></h1></div>
-    <div id="hb" class="hawlockbody" > 
+        <div id="hh" class="hawlockhead"><img src="../../public/img/image.png" alt="" id="logo" />
+            <h1 id="title">FITNESS CENTRE<br> <span id="city">RESERVATION</span></h1>
+        </div>
+        <div id="hb" class="hawlockbody">
+            <div class="divPopupModel">
 
-    </div> <!-- .hawlockbody div closed here -->
-</div> <!-- .expand div closed here -->
+                <button id="model-btn" class="purplebutton">Reserve Now</button>
+                <p id="answer"></p>
+
+                <div id="myCanvasNav" class="overlay" style="width: 0%; opacity: 0;"></div>
+                <div id="model">
+
+                    
+                    <div style="text-align: center;">
+                        <h3>Reservation details<i class="fa fa-calendar-plus"></i></i></h3><a href="javascript:void(0)" id="closebtn" style="right:0">&times;</a>
+                    </div>
+                    <form action="#" class="reservationtime" method="GET">
+                    <div id="col1">
+                            <label for="type">Start Time</label><br>
+                            <select id="starttime" name="endtime" class="input-field" placeholder="Start Time">
+                                <option value="">Select Time</option>
+                                <?php
+                                for ($hours = 6; $hours < 24; $hours++) {
+                                    for ($mins = 0; $mins < 60; $mins += 30) {
+                                ?>
+                                        <option value="endtime"><?php echo str_pad($hours, 2, '0', STR_PAD_LEFT) . ":" . str_pad($mins, 2, '0', STR_PAD_LEFT); ?></option>
+                                <?php
+                                    }
+                                }
+                                ?>
+                            </select><br>
+                            <label for="type">End Time</label><br>
+                            <select id="endtime" name="endtime" class="input-field" placeholder="End Time">
+                                <option value="">Select Time</option>
+                                <?php
+                                for ($hours = 6; $hours < 24; $hours++) {
+                                    for ($mins = 0; $mins < 60; $mins += 30) {
+                                ?>
+                                        <option value="endtime"><?php echo str_pad($hours, 2, '0', STR_PAD_LEFT) . ":" . str_pad($mins, 2, '0', STR_PAD_LEFT); ?></option>
+                                <?php
+                                    }
+                                }
+                                ?>
+                            </select>
+                        </div>
+                        <br>
+                        <input class="purplebutton"  type="submit" name="Submit" value="Reserve" style="grid-column:1">
+                    </form>
+                    <!-- <div id="btn-grp" style="grid-column: 1;">
+<button id="yes-btn">Yes</button>
+<button id="no-btn">No</button>
+</div> -->
+                </div>
+
+            </div>
+
+        </div> <!-- .hawlockbody div closed here -->
+    </div> <!-- .expand div closed here -->
 </body>
+
 </html>
