d43d90845b97b17aa5dc28301e3c055bea0300c0
Manujayad98 <n.manujayad98@gmail.com>
2021-10-11 22:23:03 +0530

    trainer profile

--- app/views/trainer/sidenav.php
+++ app/views/trainer/sidenav.php
@@ -21,7 +21,7 @@
                 <ul>
                     <li class="dropdown"><a href="#"><img src="../../uploads/profile/resident/<?php echo $_SESSION["profilePic"] ?>" onerror="this.onerror=null; this.src='../../public/img/profile.png'"></a>
                         <ul>
-                            <li><a href="#"></li><i class="fa fa-user"></i>Profile</a></li>
+                            <li><a href="profile"></li><i class="fa fa-user"></i>Profile</a></li>
                             <li><a href="../homeController/logout"><i class="fas fa-sign-out-alt"></i></i>Logout</a></li>
                         </ul>    
                             <li><a href="#">id</a></li>
