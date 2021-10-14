fff79ddeaca931b42fba5f3a83fd6256a4a7ff38
Roni-Boiz <2019cs155@stu.ucsc.lk>
2021-10-14 21:30:21 +0530

    add global edit profile function

--- app/core/controller.php
+++ app/core/controller.php
@@ -12,8 +12,43 @@ class controller{
         if(file_exists($path)){
             require $path;
             $this->model = new $modelName;
+        }  
+    }
+
+    public function editProfilePhoto()
+    {
+        if(isset($_POST) == true){
+
+            $fileType = pathinfo(basename($_FILES["file"]["name"]),PATHINFO_EXTENSION);
+            //generate unique file name
+            $fileName = $_SESSION['userName'] .'.'. $fileType;
+            //file upload path
+            if($_SESSION['type']!='resident'){
+                $targetDir = "../uploads/profile/employee/";
+            }else{
+                $targetDir = "../uploads/profile/resident/";
+            }
+            $targetFilePath = $targetDir . $fileName;
+            
+            //allow certain file formats
+            
+            $allowTypes = array('jpg','png','jpeg');
+            
+            if(in_array($fileType, $allowTypes)){
+                //upload file to server
+                if(move_uploaded_file($_FILES["file"]["tmp_name"], $targetFilePath)){
+                    $this->model->updateProfilePic($fileName,$_SESSION['userId']);
+                    $_SESSION['profilePic'] = $fileName;
+                    $response['status'] = 'ok';
+                }else{
+                    $response['status'] = 'err';
+                }
+            }else{
+                $response['status'] = 'type_err';
+            }
+            //render response data in JSON format
+            echo json_encode($response);
         }
-       
     }
 
 
