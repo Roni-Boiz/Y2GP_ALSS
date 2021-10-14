f492a6f3b6d65a712881ab5e91e54bd8cc24f9f7
Roni-Boiz <2019cs155@stu.ucsc.lk>
2021-10-14 21:49:56 +0530

    add manager announcement view

--- app/views/manager/announcementView.php
+++ app/views/manager/announcementView.php
@@ -1,46 +1,98 @@
-<?php 
-    include_once 'sidenav.php';
+<?php
+include_once 'sidenav.php';
 ?>
-<style>
-.announcement{
-    grid-column: 1/span 3;
-    box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2);
-    width: 100%;
-    margin: auto;
-    margin-top: 10px;
-    text-align: center;
-    font-family: arial;
-}
-.announcement .title{
-    color: grey;
-    font-size: 15px;
-}
-</style>
 </head>
+
 <body style="background-color: gray; background-image:none;">
-<div style="display:grid;grid-template-columns:230px 1fr" id="expand" class="content">
 
-    <div id="hh" class="hawlockhead" ><img src="../../public/img/image.png" alt="" id="logo"/><h1 id="title">Hawlock <span id="city">City</span></h1></div>
-    <div id="hb" class="hawlockbody" > 
-    <h2>Announcements</h2>
+    <div style="display:grid;grid-template-columns:230px 1fr" id="expand" class="content">
 
-    <?php
-        if ($this->ann->num_rows > 0){
-                while($row = $this->ann->fetch_assoc()){?> 
-                        <div class="announcement">
-                            <h3>By <?php echo ($row["employee_id"] == 1) ? $row["employee_id"] : $row["admin_id"];?></h3>
-                            <p class="title"><?php echo $row["topic"]; ?></p>
-                            <p><?php echo $row["content"]; ?> </p>
+        <div id="hh" class="hawlockhead">
+            <img src="../../public/img/image.png" alt="" id="logo" />
+            <h1 id="title">Hawlock <span id="city">City</span></h1>
+
+        </div>
+
+        <div id="hb" class="hawlockbody">
+            <h2>Announcements</h2>
+            <div class="btnAddAnnouncement" onclick="openNav(); openOffcanvas()" title="Add Anouncement"><img src="../../public/img/announcement.png"></div><!--<i class="fa fa-plus-square"></i>-->
+            <!-- Add Anouncement -->
+            <div id="myCanvasNav" class="overlay" onclick="closeOffcanvas()" style="width: 0%; opacity: 0;"></div>
+
+            <div id="mySidenavform" class="sidenavform">
+                <a href="javascript:void(0)" class="closebtn" onclick="closeOffcanvas()">&times;</a>
+
+                <form action="addAnnouncement" class="formAddAnnouncement" method="POST" enctype="multipart/form-data">
+                    <h1>New Announcement<i class="fa fa-bullhorn"></i></h1>
+
+                    <label for="topic">Topic</label><br>
+                    <input type="text" id="topic" name="topic" class="input-field" placeholder="New Announcement" required><br>
+
+                    <label for="content">Content</label><br>
+                    <textarea id="content" name="content" cols="30" rows="5" placeholder="Your content" required></textarea><br>
+
+                    <label for="visibility">Visibility</label><br>
+                    <div class="visibility">
+                        <label for="resient">Resident</label><br>
+                        <input type="radio" id="resident" name="visibility" value="resident" required>
+                        <label for="admin">Administration</label><br>
+                        <input type="radio" id="administration" name="visibility" value="administration" required>
+                        <label for="both">Both</label><br>
+                        <input type="radio" id="both" name="visibility" value="both" required>
+                    </div>
+
+                    <label for="file">Add File</label><br>
+                    <input type="file" id="file" name="files[]">
+                    <br><br>
+
+                    <input type="submit" name="broadcast" value="Broadcast">
+                </form>
+            </div>
+            
+            <!-- Loading Announcements -->
+            <?php
+            if ($this->ann->num_rows > 0) {
+                $count = 4;
+                while ($row = $this->ann->fetch_assoc()) {
+            ?>
+                    <div class="card">
+                        <div class="card-body">
+                            <div class="detail">
+                                <img src="../../public/img/user.png" alt="user" />
+                                <div class="detail-info">
+                                    <?php
+                                    $datetime =  $row["date"];
+                                    $date = date('Y-m-d', strtotime($datetime));
+                                    $time = date('H:i:s', strtotime($datetime));
+                                    ?>
+                                    <h5><?php echo  $date?></h5>
+                                    <small><?php echo $time ?></small>
+                                </div>
+                            </div>
+                            <h4><?php echo $row["topic"] ?></h4>
+                            <p><?php echo $row["content"] ?></p>
                         </div>
-    <?php
-                }   
-        }else{
-            echo "0 results";
-        }
-    ?>
-    <br>
-    
-    </div> <!-- .hawlockbody div closed here -->
-</div> <!-- .expand div closed here -->
+                        <?php
+                        if ($row["file_name"]) {
+                        ?>
+                            <div class="card-header">
+                                <img src="../../uploads/announcement/<?php echo $row["file_name"] ?>">
+                            </div>
+                        <?php
+                        }
+                        ?>
+                    </div>
+            <?php
+                }
+            } else {
+                echo "0 results";
+            }
+            ?>
+            <br>
+
+
+
+        </div> <!-- .hawlockbody div closed here -->
 </body>
+
 </html>
