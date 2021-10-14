36d48d1021accf0cb2b960303d05537cf50a2988
Roni-Boiz <2019cs155@stu.ucsc.lk>
2021-10-14 21:45:37 +0530

    add update password function to parent model

--- app/core/model.php
+++ app/core/model.php
@@ -15,7 +15,7 @@ class model{
         require_once 'database.php';
         return $conn;
     }
-    
+
     public function getAnnouncement(){
         if($_SESSION['type']=="resident"){
             $sql = "SELECT * FROM announcement WHERE category='resident' OR category='both' ORDER BY date DESC";
@@ -32,5 +32,34 @@ class model{
         $stmt = $this->conn->prepare($sql);
         $stmt->bind_param("ss", $fileName, $id);
         return $stmt->execute();
+    }
+
+    public function updatePassword($opw,$npw,$rnpw,$id){
+        $opw = $this->conn->real_escape_string($opw);
+        $npw = $this->conn->real_escape_string($npw);
+        $rnpw = $this->conn->real_escape_string($rnpw);
+
+        $errors=array();
+
+        $sql = "SELECT password from user_account WHERE user_id='{$id}'";
+        $oldpw = $this->conn->query($sql)->fetch_assoc();
+        $cpw = $oldpw["password"];
+        $hashPassword = sha1($opw);
+        $hash2Password = sha1($hashPassword);
+        if($hash2Password==$cpw){
+            if($npw==$rnpw){
+                $hashPassword = sha1($rnpw);
+                $hash2Password = sha1($hashPassword);
+                $sql = "UPDATE user_account SET password = ? WHERE user_id = ?";
+                $stmt = $this->conn->prepare($sql);
+                $stmt->bind_param("si",$hash2Password, $id);
+                $stmt->execute();
+                $stmt->close();            
+            }else{
+                $errors[]="doesn't match new passwords";
+            }     
+        }else{
+            $errors[]="doesn't match with previous password";
+        }
     }
 }
