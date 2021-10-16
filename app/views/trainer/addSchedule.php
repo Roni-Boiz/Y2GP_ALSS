d0324df47a0673d3c25978edcef4f072d47bc30d
Yashi6633 <nishathyasintha@gmail.com>
2021-10-16 15:27:33 +0530

    add schedule page created

---
+++ app/views/trainer/addSchedule.php
@@ -0,0 +1,26 @@
+<?php
+include_once 'sidenav.php';
+?>
+
+<style>
+
+       
+</style>
+</head>
+<body style="background-color: gray; background-image:none;">
+<div style="display:grid;grid-template-columns:230px 1fr" id="expand" class="content">
+
+    <div id="hh" class="hawlockhead" ><img src="../../public/img/image.png" alt="" id="logo"/><h1 id="title">Reservations </h1></div>
+    <div id="hb" class="hawlockbody" > 
+
+    </div> <!-- .hawlockbody div closed here -->
+</div> <!-- .expand div closed here -->
+<script>
+function clickDate(id) {
+    console.log(id);
+}
+
+</script>
+</body>
+</html>
+
