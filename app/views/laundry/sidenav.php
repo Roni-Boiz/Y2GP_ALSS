366ef10956293a4df1cb930d3534eea6a6fc9919
Manujayad98 <n.manujayad98@gmail.com>
2021-10-17 00:07:28 +0530

    landry password js

--- app/views/laundry/sidenav.php
+++ app/views/laundry/sidenav.php
@@ -10,7 +10,7 @@
     <link rel="stylesheet" href="../../public/css/body.css">
     <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Sofia">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css" />
-
+    
     <link rel="icon" href="../../favicon.ico" type="image/x-icon">
 </head>
 
@@ -72,3 +72,4 @@
                 }
             }
         </script>
+        <script src="../../public/js/laundry.js"></script>
