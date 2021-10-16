caa58450cdc956ddf8ec92769acada059d9340ef
Chathura <chathura.madura11@gmail.com>
2021-10-16 21:44:31 +0530

    change view

--- app/views/resident/billView.php
+++ app/views/resident/billView.php
@@ -10,35 +10,98 @@ include_once 'sidenav.php';
             <h1 id="title">Bill <span id="city"></span></h1>
         </div>
         <div id="hb" class="hawlockbody">
-            <div class="card">
-                <form action="#" class="reservationtime" method="GET">
-                    <div id="">
-                        <label>Year</label><br>
-                        <select name="year" class="input-field">
-                            <option>Select Year</option>
-                            <option>2019</option>
-                            <option>2020</option>
-                            <option>2021</option>
-                        </select><br>
-                        <label for="type">Month</label><br>
-                        <select name="month" class="input-field">
-                            <option value="">Select Month</option>
-                            <!-- to get time slots -->
+
+            <div id="employeeCard" style="grid-column:1/span 3">
+                <div class="staffDetails">
+                    <h4>Your Bills - October</h4>
+                    <div class="card" id="employeeSummary">
+                        
+                        <section class="wrapper" style="margin:auto">
+                            <main class="row title">
+                                <ul>
+                                    <li>Description</li>
+                                    <li>Fee (LKR)</li>
+                                    <li>Date Added</li>
+                                </ul>
+                            </main>
+
                             <?php
-                            for ($i = 0; $i < 12; $i++) {
-                                $time = strtotime(sprintf('%d months', $i));
-                                $label = date('F', $time);
-                                $value = date('n', $time); ?>
-                                <option><?php echo $value; ?></option>
+                            if ($this->bill->num_rows > 0) { ?>
+                                <?php
+                                while ($row = $this->bill->fetch_assoc()) {
+                                ?>
+                                <span id="searchrow">
+                                    <article class="row mlb">
+                                        <ul>
+                                            <li><?php echo $row["description"]; ?></li>
+                                            <li><?php echo $row["fee"]; ?></li>
+                                            <li><?php echo $row["date"]?></li>
+                                        </ul>
+                                        <ul class="more-content">
+                                            <li>
+                                                <span style="padding-right: 20px;">Other : </span>
+                                            </li>
+                                        </ul>
+                                    </article>
+                                </span>
+                                <?php
+                                }
+                                ?>
                             <?php
+                            } else {
+                                echo "0 results";
                             }
                             ?>
-                        </select><br>
-                        <input class="purplebutton" type="submit" name="Submit" value="View" style="grid-column:1">
+                            <main class="row title">
+                                <ul>
+                                    <li>Total</li>
+                                    <li></li>
+                                    <li><?php echo $row["total"]?></li>
+                                </ul>
+                            </main>
+                        </section>
+                        
+                            
+                        
+
                     </div>
-                </form>
+                </div>
+
+                <div class="staffPlotDetaills card">
+                <h4>Previous Bills</h4>
+                    <form action="#" class="reservationtime" method="GET">
+                        <div id="">
+                            <label>Year</label><br>
+                            <select name="year" class="input-field">
+                                <option>Select Year</option>
+                                <option>2019</option>
+                                <option>2020</option>
+                                <option>2021</option>
+                            </select><br>
+                            <label for="type">Month</label><br>
+                            <select name="month" class="input-field">
+                                <option value="">Select Month</option>
+                                <!-- to get time slots -->
+                                <?php
+                                for ($i = 0; $i < 12; $i++) {
+                                    $time = strtotime(sprintf('%d months', $i));
+                                    $label = date('F', $time);
+                                    $value = date('n', $time); ?>
+                                    <option><?php echo $value; ?></option>
+                                <?php
+                                }
+                                ?>
+                            </select><br>
+                            <input class="purplebutton" type="submit" name="Submit" value="View" style="grid-column:1">
+                        </div>
+                    </form>
+                </div>
+
+
             </div>
-        </div>
+
+        </div> <!-- .hawlockbody div closed here -->
+    </div>
 
     </div> <!-- .hawlockbody div closed here -->
     </div> <!-- .expand div closed here -->
--- app/views/resident/fitnessCentreView.php
+++ app/views/resident/fitnessCentreView.php
@@ -14,39 +14,95 @@ include_once 'sidenav.php';
         </div>
         <div id="hb" class="hawlockbody">
 
-            <div class="card1" style="grid-column:1/span2;margin:auto">
-                <div class="data">
-                    <div class="photo" style="background-image:url(../../public/img/gym.jpg);"></div>
-                    <ul class="details">
-                        <?php date_default_timezone_set("Asia/Colombo"); ?>
-                        <li class="author"><?php echo date("H:i"); ?> </li>
-                        <li class="date"><?php echo  date("F j, Y");  ?></li>
-                    </ul>
-                </div>
-                <div class="description">
-                    <form action="#" class="reservationtime" method="GET">
-                        <div id="">
-                            <label>Date</label><br>
-                            <input type="date" name="date" class="input-field"><br>
-                            <label>Coach</label><br>
-                            <select name="type" class="input-field">
-                                <option value="">Select coach</option>
-                                <option value="">coach 1</option>
-                                <option value="">coach 1</option>
-                                <option value="">coach 1</option>
-                                <option value="">coach 1</option>
-                            </select><br>
-                            <input class="purplebutton" type="submit" value="View" style="grid-column:2"><br><br>
-                            <div id="available">
-                                <h3>Reservations of the day</h3><br>6:00 - 6:30 4/5<br>6:30 - 7:00 4/5<br>11:00 - 12:30 4/5<br>12:30 - 2:00 4/5
+            <div class="card" id="userCard" style="z-index:0">
+                <div class="leftPanel" style="margin-top:30px">
+                    <div>
+                        <div class="card1" style="grid-column:1/span2;margin:auto">
+                            <div class="data">
+                                <div class="photo" style="background-image:url(../../public/img/gym.jpg);"></div>
+                                <ul class="details">
+                                    <?php date_default_timezone_set("Asia/Colombo"); ?>
+                                    <li class="author"><?php echo date("H:i"); ?> </li>
+                                    <li class="date"><?php echo  date("F j, Y");  ?></li>
+                                </ul>
                             </div>
-                            <br>
-                            <button id="model-btn" class="purplebutton">Reserve Now</button>
+                            <div class="description">
+                                <form action="#" class="reservationtime" method="GET">
+                                    <div id="">
+                                        <label>Date</label><br>
+                                        <input type="date" name="date" class="input-field"><br>
+                                        <label>Coach</label><br>
+                                        <select name="type" class="input-field">
+                                            <option value="">Select coach</option>
+                                            <option value="">coach 1</option>
+                                            <option value="">coach 1</option>
+                                            <option value="">coach 1</option>
+                                            <option value="">coach 1</option>
+                                        </select><br>
+                                        <input class="purplebutton" type="submit" value="View" style="grid-column:2"><br><br>
+                                        <div id="available">
+                                            <h3>Reservations of the day</h3><br>6:00 - 6:30 4/5<br>6:30 - 7:00 4/5<br>11:00 - 12:30 4/5<br>12:30 - 2:00 4/5
+                                        </div>
+                                        <br>
+                                        <button id="model-btn" class="purplebutton">Reserve Now</button>
 
+                                    </div>
+                                </form>
+                            </div>
                         </div>
-                    </form>
+                    </div>
+
                 </div>
+
+                <div class="rightPanel" style="margin-top:30px">
+                    <div class="holdAccount">
+                        <div class="head">
+                            <h3>Upcoming Reservations</h3>
+                        </div>
+                        <div class="detail">
+                            <div>
+                                <div class="detail-info">
+                                    <h5>2021-10-28 - 16:00</h5>
+                                    <small>Coach Kasun</small>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="detail">
+                            <div>
+                                <div class="detail-info">
+                                    <h5>2021-10-30 - 10:00</h5>
+                                    <small>Coach Kasun</small>
+                                </div>
+                            </div>
+                        </div>
+
+                    </div>
+                    <br>
+                    <div class="activeUsers">
+                        <div class="head">
+                            <h3>Coach List</h3>
+                        </div>
+                        <div class="detail">
+                            <img src="../../public/img/user.png" alt="user" />
+                            <div class="detail-info">
+                                <h5>Nimal Ranathunga</h5>
+                                <small>TR001</small>
+                            </div>
+                        </div>
+                        <div class="detail">
+                            <img src="../../public/img/user.png" alt="user" />
+                            <div class="detail-info">
+                                <h5>Kasun Silva</h5>
+                                <small>TR002</small>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+
             </div>
+
+
 
             <div class="divPopupModel">
                 <div id="myCanvasNav" class="overlay" style="width: 0%; opacity: 0;"></div>
