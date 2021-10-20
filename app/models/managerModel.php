cebe0fd6c997415e6ee8b3a3a39d62f421c1d7f2
Roni-Boiz <2019cs155@stu.ucsc.lk>
2021-10-20 17:05:21 +0530

    add get All reservations and get todays reservations functions

--- app/models/managerModel.php
+++ app/models/managerModel.php
@@ -44,11 +44,39 @@ class managerModel extends model {
         return $stmt->execute();
     }
 
-    public function getAllCOmplaints(){
-        $sql = "SELECT complaint.*,resident.fname,resident.lname,resident.apartment_no FROM complaint left join resident on complaint.resident_id = resident.resident_id";
+    public function getAllComplaints(){
+        $sql = "SELECT complaint.*,resident.fname,resident.lname,resident.apartment_no FROM complaint LEFT JOIN resident ON complaint.resident_id = resident.resident_id";
         return $this->conn->query($sql);
     }
 
+    public function getTodayHallRes(){
+        $sql = "SELECT hall_reservation.*,resident.fname,resident.lname FROM hall_reservation LEFT JOIN resident ON hall_reservation.resident_id=resident.resident_id WHERE cancelled_time IS NULL AND date = CURDATE() ORDER BY date,start_time";
+        return $this->conn->query($sql);
+    }
+
+    public function getTodayFitnessRes(){
+        $sql = "SELECT fitness_centre_reservation.*,resident.fname AS rfname,resident.lname AS rlname,trainer.fname AS tfname, trainer.lname AS tlname FROM fitness_centre_reservation LEFT JOIN resident on fitness_centre_reservation.resident_id=resident.resident_id LEFT JOIN trainer ON fitness_centre_reservation.employee_id=trainer.employee_id WHERE cancelled_time IS NULL AND date = CURDATE() ORDER BY date,start_time";
+        return $this->conn->query($sql);
+    }
+
+    public function getTodayTreatmentRes(){
+        $sql = "SELECT treatment_room_reservation.*,resident.fname AS rfname,resident.lname AS rlname,treater.fname AS tfname, treater.lname AS tlname FROM treatment_room_reservation LEFT JOIN resident ON treatment_room_reservation.resident_id=resident.resident_id LEFT JOIN treater ON treatment_room_reservation.employee_id=treater.employee_id WHERE cancelled_time IS NULL AND date = CURDATE() ORDER BY date,start_time";
+        return $this->conn->query($sql);
+    }
     
+    public function getAllHallRes(){
+        $sql = "SELECT hall_reservation.*,resident.fname,resident.lname FROM hall_reservation LEFT JOIN resident on hall_reservation.resident_id=resident.resident_id ORDER BY reserved_time DESC";
+        return $this->conn->query($sql);
+    }
+
+    public function getAllFitnessRes(){
+        $sql = "SELECT fitness_centre_reservation.*,resident.fname AS rfname,resident.lname AS rlname,trainer.fname AS tfname, trainer.lname AS tlname FROM fitness_centre_reservation LEFT JOIN resident ON fitness_centre_reservation.resident_id=resident.resident_id LEFT JOIN trainer ON fitness_centre_reservation.employee_id=trainer.employee_id ORDER BY reserved_time DESC";
+        return $this->conn->query($sql);
+    }
+
+    public function getAllTreatmentRes(){
+        $sql = "SELECT treatment_room_reservation.*,resident.fname AS rfname,resident.lname AS rlname,treater.fname AS tfname, treater.lname AS tlname FROM treatment_room_reservation LEFT JOIN resident ON treatment_room_reservation.resident_id=resident.resident_id LEFT JOIN treater ON treatment_room_reservation.employee_id=treater.employee_id ORDER BY reserved_time DESC";
+        return $this->conn->query($sql);  
+    }
 
 }
