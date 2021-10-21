b5391b77b1ea4ebb3c34cc1ca70d386dc4d067fb
Yashi6633 <nishathyasintha@gmail.com>
2021-10-21 19:14:54 +0530

    add schedule view

--- app/views/trainer/addScheduleView.php
+++ app/views/trainer/addScheduleView.php
@@ -3,8 +3,67 @@ include_once 'sidenav.php';
 ?>
 
 <style>
+    .addSceduleBody{
+        grid-column: 1/span3;
+        display: grid;
+    }
+    
+#addScedule{
+    padding: 50px;
+    display: flex;
+    flex-wrap:wrap;
+    align-items:center;
+    color: #545d7a;
+}
+.addSceduleSig{
+    text-align: center;
+    justify-content: center;
+    align-items: center;
+    width: 30%;
+    max-width: 300px;
+    min-width: 200px;
+    margin: 10px;
+    border-radius: 10px;
+    box-shadow: 0 4px 8px 0 rgb(0 0 0 / 66%);
+    border: 1px solid #898484;
+}
+.addSceduleList{
+    grid-column:2;
+    text-align: center;
+    justify-content: center;
+    align-items: center;
+    width: 65%;
+    max-width: 1000px;
+    margin: 10px;
+    border-radius: 10px;
+    box-shadow: 0 4px 8px 0 rgb(0 0 0 / 66%);
+    border: 1px solid #898484;
+    display:grid;
+    grid-template: 1fr 1fr;
+}
+.addSceduleSig i {
+    font-size: 80px;
+}
+.addSceduleList .title{
+    grid-column: 1 / span 2 ;
+    font-size:1.17em;
+}
+.addSceduleList .leftList{
+    grid-column:1;
+    padding:20px;
+    text-align:left;
+}
+.addSceduleList .rightList{
+    grid-column:2;
+    padding:20px;
+}
+.addSceduleList p{
+    font-weight: 900;
+    
+    padding:10px;
+    margin-right: 60px;
+}
 
-       
 </style>
 </head>
 <body style="background-color: gray; background-image:none;">
@@ -12,6 +71,65 @@ include_once 'sidenav.php';
 
     <div id="hh" class="hawlockhead" ><img src="../../public/img/image.png" alt="" id="logo"/><h1 id="title">ADD SCHEDULE</h1></div>
     <div id="hb" class="hawlockbody" > 
+
+    <div class="addSceduleBody">
+    <div class="addScheduleButton">
+    <button>Add Schedule</button>
+</div>
+    <div class="card" id="addScedule">
+
+<div class="addSceduleSig">
+    <div>
+        <span><i class="fas fa-briefcase"></i></span>
+        <h3>Today Reservations</h3>
+    </div>
+    <div style="font-size: 40px;">
+        100
+    </div>
+</div>
+
+<div class="addSceduleList">
+    <div class="title">
+        <h3>
+            Reservations
+        </h3>
+    </div>
+    <div class="leftList">
+        <p>08:00-09:00 - 0</p>
+        <p>09:00-10:00 - 1</p>
+        <p>10:00-11:00 - 5</p>
+        <p>08:00-09:00 - 4</p>
+        <p>09:00-10:00 - 3</p>
+        <p>10:00-11:00 - 1</p>
+        <p>08:00-09:00 - 4</p>
+        <p>09:00-10:00 - 3</p>
+        <p>10:00-11:00 - 1</p>
+        <p>08:00-09:00 - 4</p>
+        <p>09:00-10:00 - 3</p>
+        <p>10:00-11:00 - 1</p>
+        
+    </div>
+    <div class="rightList">
+        <p>08:00-09:00 - 4</p>
+        <p>09:00-10:00 - 3</p>
+        <p>10:00-11:00 - 1</p>
+        <p>08:00-09:00 - 4</p>
+        <p>09:00-10:00 - 3</p>
+        <p>10:00-11:00 - 1</p>
+        <p>08:00-09:00 - 4</p>
+        <p>09:00-10:00 - 3</p>
+        <p>10:00-11:00 - 1</p>
+        <p>08:00-09:00 - 4</p>
+        <p>09:00-10:00 - 3</p>
+        <p>10:00-11:00 - 1</p>
+        
+    </div>
+</div>
+
+</div>
+
+    </div>
+                        
 
     </div> <!-- .hawlockbody div closed here -->
 </div> <!-- .expand div closed here -->
