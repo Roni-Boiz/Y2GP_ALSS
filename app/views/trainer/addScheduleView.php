c75df4c8bcca632ecaadae83a8cb3f67ea5e5816
Yashi6633 <nishathyasintha@gmail.com>
2021-10-21 12:14:45 +0530

    add schedule view created

--- app/views/trainer/addScheduleView.php
+++ app/views/trainer/addScheduleView.php
@@ -10,7 +10,7 @@ include_once 'sidenav.php';
 <body style="background-color: gray; background-image:none;">
 <div style="display:grid;grid-template-columns:230px 1fr" id="expand" class="content">
 
-    <div id="hh" class="hawlockhead" ><img src="../../public/img/image.png" alt="" id="logo"/><h1 id="title">Reservations </h1></div>
+    <div id="hh" class="hawlockhead" ><img src="../../public/img/image.png" alt="" id="logo"/><h1 id="title">ADD SCHEDULE</h1></div>
     <div id="hb" class="hawlockbody" > 
 
     </div> <!-- .hawlockbody div closed here -->
